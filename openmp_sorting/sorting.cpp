18e919df180f6c6ed64114de3f3762778284c5a0
Grzegorz Borkowski <gb.grzegorzborkowski@gmail.com>
2018-05-08 20:40:41 +0200

    OpenMP init version

---
+++ openmp_sorting/sorting.cpp
@@ -0,0 +1,103 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <omp.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char**argv) {
+        if (argc < 4) {
+                cout << "Wincyj argumentow kolezko!" << endl;
+                return -1;
+        }
+        int number_of_buckets = 7;
+        int size = stoi(argv[1]);
+        int elementTable[size];
+        bool print_array = stoi(argv[2]);
+        bool print_buckets = stoi(argv[3]);
+        int range_of_numbers = 500;
+
+        vector<int> buckets[number_of_buckets];
+        omp_lock_t write_locks[number_of_buckets];
+
+        for(int i=0; i<number_of_buckets; i++) {
+                omp_init_lock(&write_locks[i]);
+        }
+       // TODO: poprawic randa na cos innego, rand blokuje watki!!!
+        #pragma omp parallel for
+        for(int n=0; n<size; ++n) {
+                int rand_number = rand() % range_of_numbers;
+                elementTable[n] = rand_number;
+        }
+
+        if (print_array) {
+                for(int i=0; i<size; i++) {
+                        cout << elementTable[i] << " ";
+                }
+        }
+
+        #pragma omp parallel for
+        for(int n=0; n<size; ++n) {
+                int bucket_number = elementTable[n]/(range_of_numbers/number_of_buckets);
+                bucket_number = min(bucket_number, number_of_buckets-1);
+                omp_set_lock(&write_locks[bucket_number]);
+                buckets[bucket_number].push_back(elementTable[n]);
+                omp_unset_lock(&write_locks[bucket_number]);
+        }
+
+        for(int i=0; i<number_of_buckets; i++) {
+                omp_destroy_lock(&write_locks[i]);
+        }
+
+        if (print_buckets) {
+                for(int i=0; i<number_of_buckets; i++) {
+                        for(int j=0; j<buckets[i].size(); j++) {
+                                cout << buckets[i][j] << " ";
+                        }
+                        cout << endl;
+                }
+        }
+
+        #pragma omp parallel for
+        for(int i=0; i<number_of_buckets; i++) {
+                sort(buckets[i].begin(), buckets[i].end());
+        }
+
+        vector<int> offset_vector;
+        int current_sum = 0;
+
+        for(int i=0; i<number_of_buckets; i++) {
+                int bucket_size = buckets[i].size();
+                current_sum += bucket_size;
+                offset_vector.push_back(current_sum);
+        }
+
+        for(int j=0; j<offset_vector.size(); j++) {
+                cout << offset_vector[j] << " ";
+        }
+
+        vector<int> result_vector(current_sum);
+
+        #pragma omp parallel for
+        for(int i=0; i<number_of_buckets; i++) {
+                int get_offset_start;
+                if (i==0) {
+                        get_offset_start = 0;
+                } else {
+                get_offset_start = offset_vector[i-1];
+                }       
+                int get_offset_end = offset_vector[i];
+
+                int element_in_bucket_idx = 0;
+
+                for(int k=get_offset_start; k<get_offset_end; k++) {
+                        result_vector[k] = buckets[i][element_in_bucket_idx];
+                        element_in_bucket_idx+=1;
+                }
+        }
+        return 0;
+
+}
